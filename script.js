a93f624bb452ae7af34d8327f2274a8727933ed4
ronycastroc <castroroony@gmail.com>
2022-06-11 20:41:26 -0300

    botaofinalizar

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
-let pra;
+let pra;  
 let beb;
 let pud;
 
 function clicou(pratoselec) {
     let click = document.querySelector('.borda')
+    pra = pratoselec.innerHTML
+
     if (click !== null) {
         click.classList.remove('borda')
     }
     pratoselec.classList.add('borda')
+    
+    liberar()
 }
 
 function clicou2(bebidaselec) {
     let click = document.querySelector('.bebidaclick .borda')
+    beb = bebidaselec.innerHTML
     if (click !== null) {
         click.classList.remove('borda')
     }
     bebidaselec.classList.add('borda')
+
+    liberar()
 }
 
 function clicou3(pudimselec) {
     let click = document.querySelector('.pudimclick .borda')
+    pud = pudimselec.innerHTML
     if (click !== null) {
         click.classList.remove('borda')
     }
+    pudimselec.classList.add('borda') 
 
-    pudimselec.classList.add('borda')
+    liberar()
 }
 
+function liberar() {
     if (pra && beb && pud) {
+        let p = document.querySelector('.botaobarra p')
         let botao = document.querySelector('.botaobarra')
+        p.innerHTML = 'Finalizar'
         botao.classList.add('selecionado')
+    }
 }
+
+
+function finalizar() {
+    
+    if (pra && beb && pud) {
+          alert('compra ai')
+}
+}
+    
