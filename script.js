ac56a16dad2cf4862e2bb9268884c99e92ca5138
ronycastroc <castroroony@gmail.com>
2022-06-11 01:30:57 -0300

    menuclicavel

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-function clicou() {
-    let click = document.querySelector('.opcaoprato')
-    click.classList.toggle('borda')
-    
-    
+let pra;
+let beb;
+let pud;
+
+function clicou(pratoselec) {
+    let click = document.querySelector('.borda')
+    if (click !== null) {
+        click.classList.remove('borda')
+    }
+    pratoselec.classList.add('borda')
+}
+
+function clicou2(bebidaselec) {
+    let click = document.querySelector('.bebidaclick .borda')
+    if (click !== null) {
+        click.classList.remove('borda')
+    }
+    bebidaselec.classList.add('borda')
+}
+
+function clicou3(pudimselec) {
+    let click = document.querySelector('.pudimclick .borda')
+    if (click !== null) {
+        click.classList.remove('borda')
+    }
+
+    pudimselec.classList.add('borda')
+}
+
+    if (pra && beb && pud) {
+        let botao = document.querySelector('.botaobarra')
+        botao.classList.add('selecionado')
 }
